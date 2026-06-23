567eae1bedfd039d6cf5f08f806de33ddba5ebe8
soheldev.codes <soheldev.codes@gmail.com>
2026-06-23 16:38:26 +0600

    stripe payment method  connected

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
+const Stripe = require("stripe");
+
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
